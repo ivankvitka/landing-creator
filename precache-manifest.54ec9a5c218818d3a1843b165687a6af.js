4d1f2ef94f9de1e1dc088d7cbea66a39d2da3f91
Ivan.Kvitka <ikvitka@deloitte.com>
2020-07-13 12:14:05 +0300

    Updates

---
+++ precache-manifest.54ec9a5c218818d3a1843b165687a6af.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c280e0453defa5e5a704ea7ff50dcc",
+    "url": "/landing-creator/index.html"
+  },
+  {
+    "revision": "b00cc7fec387c564448e",
+    "url": "/landing-creator/static/js/2.335b8a9e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/landing-creator/static/js/2.335b8a9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eacd295735251b14c7e",
+    "url": "/landing-creator/static/js/main.ba9907c7.chunk.js"
+  },
+  {
+    "revision": "3067581ea85707bd6603",
+    "url": "/landing-creator/static/js/runtime-main.fc8b0260.js"
+  },
+  {
+    "revision": "08cb8f79715774f9a6285ee7db2919a3",
+    "url": "/landing-creator/static/media/Roboto-Bold.08cb8f79.woff"
+  },
+  {
+    "revision": "16d7bb99c6f81cacdd91cd92d8ddb545",
+    "url": "/landing-creator/static/media/Roboto-Bold.16d7bb99.ttf"
+  },
+  {
+    "revision": "4a1d8c27dac2c1bdee46fcfb5c80089b",
+    "url": "/landing-creator/static/media/Roboto-Bold.4a1d8c27.eot"
+  },
+  {
+    "revision": "f3a02e2578bee50e620e515912278bc9",
+    "url": "/landing-creator/static/media/Roboto-Bold.f3a02e25.woff2"
+  },
+  {
+    "revision": "ad4e08740474893c8f08df87268544ce",
+    "url": "/landing-creator/static/media/Roboto-Italic.ad4e0874.woff2"
+  },
+  {
+    "revision": "b391d0e9dd86ead6604092b134e407bf",
+    "url": "/landing-creator/static/media/Roboto-Italic.b391d0e9.ttf"
+  },
+  {
+    "revision": "d798f933ca2a3fa8d39fb3ac80cea706",
+    "url": "/landing-creator/static/media/Roboto-Italic.d798f933.eot"
+  },
+  {
+    "revision": "fd5970128586cba8e7ed994f134ec176",
+    "url": "/landing-creator/static/media/Roboto-Italic.fd597012.woff"
+  },
+  {
+    "revision": "4312f1fbdcf4d54af4506dabdce08010",
+    "url": "/landing-creator/static/media/Roboto-Regular.4312f1fb.ttf"
+  },
+  {
+    "revision": "94dac78eee406a8c8f0406b69b85ac2b",
+    "url": "/landing-creator/static/media/Roboto-Regular.94dac78e.woff"
+  },
+  {
+    "revision": "9feb0110b6dff9ee2b9ebd17f7a1aee6",
+    "url": "/landing-creator/static/media/Roboto-Regular.9feb0110.woff2"
+  },
+  {
+    "revision": "b9077621ce786b55c176a61456bfc077",
+    "url": "/landing-creator/static/media/Roboto-Regular.b9077621.eot"
+  },
+  {
+    "revision": "81950ec6e3628f49028ffdd167472307",
+    "url": "/landing-creator/static/media/TradeWinds.81950ec6.woff2"
+  },
+  {
+    "revision": "ade553e44c61b38121cf7a5d4cfc8d46",
+    "url": "/landing-creator/static/media/TradeWinds.ade553e4.woff"
+  },
+  {
+    "revision": "b2357d30a19c123c2b5684b7eb301828",
+    "url": "/landing-creator/static/media/TradeWinds.b2357d30.eot"
+  }
+]);
